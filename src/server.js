0a8e11eaaf43cb13b30b069d6fb2e5b32d5e828e
Sapper <saniamykhailenko@gmail.com>
2025-11-10 17:51:59 +0200

    add 4 hw

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -17,8 +19,10 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser());
 
 app.use(notesRoutes);
+app.use(authRoutes);
 
 app.use(notFoundHandler);
 
